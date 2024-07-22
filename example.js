ccda00e35f191e5eb7faf93481e74fc4299a22e6
shubhamvermal <shubhamverma1024@gmail.com>
2024-07-22 18:01:33 +0530

    initial commit

---
+++ example.js
@@ -0,0 +1,3 @@
+const { generateUUID } = require("./dist/index");
+const uuid = generateUUID();
+console.log("🚀 ~ file: test.js:4 ~ uuid:", uuid);
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { randomBytes } from "crypto";
+
+export function generateUUID(): string {
+  return "xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx".replace(/[xy]/g, function (c) {
+    const r = randomBytes(1)[0] % 16;
+    const v = c === "x" ? r : (r & 0x3) | 0x8;
+    return v.toString(16);
+  });
+}
